bacd02fd6e93c77bed44ec29023adac634d2e9df
Alfred Lim <alf1650@hotmail.com>
2023-10-11 22:14:16 +0800

    Heroku 2

--- controller.test.js
+++ controller.test.js
@@ -14,6 +14,6 @@ describe("the print function", ()=>{
         }
 
         targetFn(mockReq, mockRes);
-        expect(mockRes.send).toHaveBeenCalledWith("Hello world!!"); // If you change this value, the test will fail.
+        expect(mockRes.send).toHaveBeenCalledWith("Alfred successfully deployed to Heroku"); // If you change this value, the test will fail.
     })
 })
